7a79b6d6f4e3e451d549f559615d387509193282
Igor Sostarko <igor.sostarko@fer.hr>
2024-09-05 21:12:03 +0200

    implemented login endpoint using jwt, program.cs configurations and fixes, deletet start project weatherforecast.cs

--- ChatApp.Server/Controllers/AuthController.cs
+++ ChatApp.Server/Controllers/AuthController.cs
@@ -1,9 +1,16 @@
-﻿using ChatApp.Server.Dtos;
+﻿using ChatApp.Server.Auth;
+using ChatApp.Server.Dtos;
+using ChatApp.Server.Interfaces;
 using ChatApp.Server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace ChatApp.Server.Controllers
 {
@@ -13,11 +20,17 @@ namespace ChatApp.Server.Controllers
     {
         private readonly UserManager<ChatUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<ChatUser> _signInManager;
+        private readonly JwtSettings _jwtSettings;
+        private readonly IUserService _userService;
 
-        public AuthController(UserManager<ChatUser> userManager, RoleManager<IdentityRole> roleManager)
+        public AuthController(UserManager<ChatUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<ChatUser> signInManager, IOptions<JwtSettings> jwtSettings, IUserService userService)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
+            _jwtSettings = jwtSettings.Value;
+            _userService = userService;
         }
 
 
@@ -51,5 +64,60 @@ namespace ChatApp.Server.Controllers
             }
             return BadRequest();
         }
+        [HttpPost("login")]
+        public async Task<ActionResult<LoginResponseDto>> Login([FromBody] LoginDto model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return Unauthorized(new { Message = "Invalid credentials" });
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(user.UserName, model.Password, isPersistent: false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                // Generiranje JWT tokena
+                var token = GenerateJwtToken(user);
+
+                return Ok(new LoginResponseDto()
+                {
+                    Token = token,
+                    PrivateEncriptedKey = await _userService.GetEncriptedPrivateKeyOfUserWithMailAsync(user.Email),
+                    Roles = await _userManager.GetRolesAsync(user)
+                });
+            }
+
+            return Unauthorized(new { Message = "Invalid credentials" });
+        }
+
+        private string GenerateJwtToken(ChatUser user)
+        {
+            var claims = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "",
+                audience: "",
+                claims: claims,
+                expires: DateTime.Now.AddHours(3),  // Vrijeme trajanja tokena
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
     }
 }
--- ChatApp.Server/Program.cs
+++ ChatApp.Server/Program.cs
@@ -1,5 +1,8 @@
+using ChatApp.Server.Auth;
 using ChatApp.Server.Data;
+using ChatApp.Server.Interfaces;
 using ChatApp.Server.Models;
+using ChatApp.Server.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,24 +11,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddIdentityCore<ChatUser>(options =>
+builder.Services.AddIdentity<ChatUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
     options.Password.RequireLowercase = true;
     options.Password.RequireUppercase = true;
     options.Password.RequiredLength = 8;
+})
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
 
-}).AddRoles<IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(nameof(JwtSettings)));
 
 var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+app.UseMiddleware<JwtMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
