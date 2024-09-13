e06644ec39355cfba26df17470cfa11960cb3f9a
Igor Sostarko <igor.sostarko@fer.hr>
2024-09-13 20:32:43 +0200

    adding field to db for whom is message encrypted for

---
+++ ChatApp.Server/Migrations/20240913173031_added-for-which-user-is-msg-encoded.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedforwhichuserismsgencoded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedForId",
+                table: "Messages",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_EncryptedForId",
+                table: "Messages",
+                column: "EncryptedForId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_EncryptedForId",
+                table: "Messages",
+                column: "EncryptedForId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_EncryptedForId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_EncryptedForId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "EncryptedForId",
+                table: "Messages");
+        }
+    }
+}
